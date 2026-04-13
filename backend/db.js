2464e1926df292040df1caef5eea0d3b96d67da5
Leo Angelo Larre Agpoon <leoangelolarreagpoon@gmail.com>
2026-04-13 22:19:17 +0800

    Fixed and added hyperlinks

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'cscpeboy12',
+  password: '',
   database: 'EnergySync_DB'
 });
 
